9da24ec6319061e69dc6819c2dd49385bbad953f
Zienit <4760167+zienit@users.noreply.github.com>
2019-08-21 20:20:34 +0200

    examples

--- src/main/kotlin/nl/zienit/functional/Try.kt
+++ src/main/kotlin/nl/zienit/functional/Try.kt
@@ -5,6 +5,8 @@ import kotlin.reflect.full.isSuperclassOf
 
 sealed class Try<T> {
 
+    abstract fun isSuccess(): Boolean
+    abstract fun isFailure(): Boolean
     abstract fun get(): T
     abstract fun getException(): Throwable
     abstract fun <U> map(mapper: (T) -> U): Try<U>
@@ -32,6 +34,8 @@ sealed class Try<T> {
 
     class Success<T>(val value: T) : Try<T>() {
 
+        override fun isSuccess(): Boolean = true
+        override fun isFailure(): Boolean = false
         override fun get(): T = value
         override fun getException(): Throwable = throw IllegalStateException()
         override fun <U> map(mapper: (T) -> U): Try<U> = eval { success(mapper(value)) }
@@ -47,6 +51,8 @@ sealed class Try<T> {
 
     class Failure<T>(val ex: Throwable) : Try<T>() {
 
+        override fun isSuccess(): Boolean = false
+        override fun isFailure(): Boolean = true
         override fun get(): T = throw ex
         override fun getException(): Throwable = ex
         override fun <U> map(mapper: (T) -> U): Try<U> = this as Try<U>
@@ -60,4 +66,25 @@ sealed class Try<T> {
         override fun <E : Throwable> recoverWith(clazz: KClass<E>, mapper: (E) -> Try<T>): Try<T> =
             eval { if (clazz.isSuperclassOf(ex::class)) mapper(ex as E) else this }
     }
+}
+
+fun <T> T.success() = Try.success(this)
+fun <E : Throwable, T> E.failure() = Try.failure<T>(this)
+
+fun <T> Sequence<Try<T>>.toTryList(): Try<List<T>> {
+    return toCollection(this.asIterable(), mutableListOf<T>()) as Try<List<T>>
+}
+
+fun <T> Collection<Try<T>>.toTryList(): Try<List<T>> {
+    return toCollection(this.asIterable(), mutableListOf<T>()) as Try<List<T>>
+}
+
+private fun <T> toCollection(from: Iterable<Try<T>>, to: MutableCollection<T>): Try<out Collection<T>> {
+    for (item in from) {
+        if (item.isFailure()) {
+            return item as Try.Failure<List<T>>
+        }
+        to.add(item.get())
+    }
+    return to.success()
 }
---
+++ src/test/kotlin/nl/zienit/functional/ExampleTest.kt
@@ -0,0 +1,114 @@
+package nl.zienit.functional
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.hasItems
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import java.sql.SQLException
+
+data class ProgrammingLanguage(val name: String, val creator: String, val predecessor: String?)
+
+val databaseOfLanguages = listOf(
+    ProgrammingLanguage("C", "Dennis Ritchie", null),
+    ProgrammingLanguage("Java", "James Gosling", "C"),
+    ProgrammingLanguage("Kotlin", "JetBrains", "Java")
+)
+
+fun findLanguage(query: String): Try<ProgrammingLanguage> = databaseOfLanguages
+    .filter { it.name == query }
+    .map { it.success() }
+    .firstOrNull() ?: NoSuchElementException().failure()
+
+fun makeLanguageLister(reliable: Boolean): () -> Try<List<String>> = {
+    if (reliable)
+        databaseOfLanguages.map { it.name }.toList().success()
+    else
+        SQLException().failure()
+}
+
+class ExampleTest {
+
+    @Test
+    fun testFindLanguage() {
+        val kotlin = findLanguage("Kotlin")
+        val foobar = findLanguage("Foobar")
+
+        assertThat(kotlin.isSuccess(), equalTo(true))
+        assertThat(foobar.isSuccess(), equalTo(false))
+    }
+
+    @Test
+    fun testListLanguages() {
+
+        val listLanguages = makeLanguageLister(reliable = true)
+
+        val languages = listLanguages();
+
+        val greetings = languages
+            .map {
+                "greetings from ${it.joinToString(",")}"
+            }
+        assertThat(greetings.get(), equalTo("greetings from C,Java,Kotlin"))
+    }
+
+    @Test(expected = SQLException::class)
+    fun testListLanguagesFailure() {
+
+        val listLanguages = makeLanguageLister(reliable = false)
+
+        val languages = listLanguages();
+
+        val greetings = languages
+            .map {
+                "greetings from ${it.joinToString(",")}"
+            }
+        assertThat(greetings.get(), equalTo("greetings from C,Java,Kotlin"))
+    }
+
+    @Test
+    fun testListLanguagesRecover() {
+
+        val listLanguages = makeLanguageLister(reliable = false)
+
+        val languages = listLanguages();
+
+        val greetings = languages
+            .map {
+                "greetings from ${it.joinToString(",")}"
+            }
+            .recover { "sorry, something went wrong" }
+        assertThat(greetings.get(), equalTo("sorry, something went wrong"))
+    }
+
+    @Test
+    fun testFindPredecessorOfKotlin() {
+
+        val language = findLanguage("Kotlin")
+            .flatMap {
+                if (it.predecessor == null)
+                    NoSuchElementException().failure()
+                else
+                    findLanguage(it.predecessor)
+            }
+
+        assertThat(language.get().name, equalTo("Java"))
+    }
+
+    @Test
+    fun testListCreators() {
+
+        val listLanguages = makeLanguageLister(reliable = true)
+
+        val creators = listLanguages()
+            .flatMap { names ->
+                names
+                    .map { name ->
+                        findLanguage(name)
+                            .map { it.creator }
+                    }
+                    .toTryList()
+            }
+
+        assertThat(creators.get(), hasItems("Dennis Ritchie", "James Gosling", "JetBrains"))
+    }
+}
